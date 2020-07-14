3c8c6a803bf7df6c05ca6fff02a248d618a48eff
Hilda Amponsah <hilda.amp@hotmail.com>
2020-07-14 13:56:04 +0100

    max Limit 90

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,17 @@
-
 class Oystercard
  attr_reader :balance
  DEFAULT_BALANCE = 0
 
-        def initialize(balance = 0)
+        def initialize(balance = DEFAULT_BALANCE)
             @balance = balance
         end
 
+        def top_up(amount)
+            raise "amount more than 90" if @balance >= 90
+            @balance += amount
+        end
+
+        def deduct(amount)
+           @balance -= amount 
+        end
 end
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -4,10 +4,21 @@ describe Oystercard do
 
     describe "balance" do
      it "#must have no money on card" do
-        card = Oystercard.new
-        expect(card.balance).to eq 0
+        # card = Oystercard.new
+        expect(subject.balance).to eq 0
     end
-end
-    # it "#must have money on card" do
+  end
+    it "#is able to top" do
+        expect(subject.top_up(5)).to eq 5
+    end
 
+    it "#maximum limit on card is eq 90" do
+        oyster = Oystercard.new(90)
+     expect { oyster.top_up(1) }.to raise_error "amount more than 90"
+    end
+
+    # it "#deducted a certain amaount from card balance" do
+    #     subject.top_up(10)
+        
+  
 end
